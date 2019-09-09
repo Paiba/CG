a221e1b69a864fc27db8dcb8341696390dbe0d7a
Paiba <pedropaivaladeira@hotmail.com>
2019-09-08 21:26:42 -0300

    Agora desenha os círculos, mas não evita colisão ainda, arquivos do tinyxml adicionados

--- TC1/tc1.cpp
+++ TC1/tc1.cpp
@@ -5,22 +5,42 @@
 #include <iostream>
 #include <unistd.h>
 #include <list>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 using namespace std;
 #define PI 3.1415926535
 //*****FUNÇÕES QUE SERÃO UTILIZADAS*****//
 
+
+
+bool check = 0;
+
+
 int i, circle_points = 1000;
+//XML CIRCULO
 float angle, raio;
+float cR, cG,cB;
+//XML Modelo
+float cnR, cnG, cnB;
+float caR, caG, caB;
+//XML Tela
+string titulo = "TC1";
+int wzx = 500, wzy = 500;
+
+
+
 float gx = 0,gy = 0;
 float gxm = 0.05, gym = 0.05;
 float gxmf = 0.0, gymf = 0.0;
-int wzx = 500, wzy = 500;
+
+
+
 
 //vetor dos circulos
-list<float> pos_x;
-list<float> pos_y;
-list<float>::iterator itex = pos_x.begin();
-list<float>::iterator itey = pos_y.begin();
+vector<float> pos_x;
+vector<float> pos_y;
+
 
 void display(void);
 void init(void);
@@ -39,43 +59,27 @@ int main(int argc, char**argv)
 	glutInitDisplayMode(GLUT_SINGLE |GLUT_RGB);
 	glutInitWindowSize(wzx, wzy); 
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("TC1");
+	glutCreateWindow("aiai");
 	
 
-	//Callbacks
-	glutIdleFunc(idle);
-	
+	//Callbacks	
 	init();
 	glutDisplayFunc(display);
-	
-
-	//Main Loop 
+    glutMouseFunc(mouse);
+    glutPassiveMotionFunc(cursorFormat);
+	glutMotionFunc(arrasta);
+    //Main Loop 
 	glutMainLoop();
-    
 	return 0;
 }
 
-//******IDLE*********//
-
-void idle(void){
-    glutPassiveMotionFunc(cursorFormat);
-    glutMouseFunc(mouse);
-    glutPostRedisplay();
-}
 
 void display(void){
+    
+    
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0,0,1.0);
-    glBegin(GL_POLYGON);
-    glColor3f(0.13, 0.55, 40);
-    raio = 0.1;
-    for(i = 0; i < circle_points; i++) {
-        angle = (2*PI*i)/circle_points;
-        glVertex2f(gxmf+raio*cos(angle),gymf+raio*sin(angle));
-    }
-    glEnd();
-    advance(itex,1);
-    advance(itey,1);
+    
     glBegin(GL_LINE_LOOP);
 	glColor3f(0.13, 0.55, 40);
 	raio = 0.1;
@@ -84,6 +88,19 @@ void display(void){
 		glVertex2f(gxm+raio*cos(angle),gym+raio*sin(angle));
 	}
     glEnd();
+    for(int j=0; j<(pos_x.size()); j++){
+            if(check){
+                
+                    glBegin(GL_POLYGON);
+                    glColor3f(0.13, 0.55, 40);
+                    raio = 0.1;
+                    for(i = 0; i < circle_points; i++) {
+                        angle = (2*PI*i)/circle_points;
+                        glVertex2f(pos_x[j]+raio*cos(angle),pos_y[j]+raio*sin(angle));
+                    }
+                    glEnd();
+            }}
+    
 	
 	glFlush();
 }
@@ -109,17 +126,18 @@ void init (void)
 void mouse(int button, int  state, int x, int y){
        
         if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+            check = 1;
             y = wzy - y;
-            gxmf = (float) x/ wzx;
-            gymf = (float) y/ wzy;    
-            pos_x.push_back(x);
-            pos_y.push_back(y);
+            pos_x.push_back((float)x/ wzx);
+            pos_y.push_back((float)y/ wzy);
+                        
         }
-        else{
+        else if {button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
             
         }
         glutPostRedisplay();
 }
+
 
 
 
