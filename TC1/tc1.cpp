ff79410f2bdb8b7754048378270b7dbf4b3999ad
Paiba <pedropaivaladeira@hotmail.com>
2019-09-09 03:44:56 -0300

    Trabalho Curto 1 completo

--- TC1/tc1.cpp
+++ TC1/tc1.cpp
@@ -8,34 +8,34 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
-#include "tinyXml/tinystr.h"
-#include "tinyXml/tinyxml.h"
+#include "tinyxml2.h"
 
 using namespace std;
+using namespace tinyxml2;
 #define PI 3.1415926535
 //*****FUNÇÕES E VARIAVEIS QUE SERÃO UTILIZADAS*****//
 
 
-
+bool impedido = false;
 
 bool dragCirc = false;
 int i, circle_points = 1000;
 float angle;
 //XML CIRCULO
-float raio;
+int raio;
 float cR, cG,cB;
 //XML CircModelo
 float cnR, cnG, cnB;
 float caR, caG, caB;
 //XML Tela
 float telaR, telaG, telaB;
-string titulo = "TC1";
-int wzx=500 , wzy=500;
+string titulo;
+int wzx , wzy;
 
 
 
 float gx = 0,gy = 0;
-float gxm = 0.05, gym = 0.05;
+float gxm = 0.0, gym = 0.0;
 float gxmf = 0.0, gymf = 0.0;
 
 bool check = 0;
@@ -53,17 +53,56 @@ void arrasta(int, int);
 void idle(void);
 bool distancia(int , int);
 
+//Lendo XML
+
+void read_xml(char* FileName){
+	XMLDocument doc;
+	doc.LoadFile(strcat(FileName, "config.xml"));
+	XMLNode* pRoot = doc.FirstChild();
+	XMLElement* pElement = pRoot->FirstChildElement("janela");
+	pElement = pRoot->FirstChildElement("janela");
+	pElement = pElement->FirstChildElement("titulo");
+	titulo = pElement->GetText();
+    pElement = pRoot->FirstChildElement("janela");
+    pElement = pElement->FirstChildElement("dimensao");
+    pElement->QueryIntAttribute("largura",&wzx);
+	pElement->QueryIntAttribute("altura",&wzy);
+    pElement = pRoot->FirstChildElement("janela");
+    pElement = pElement->FirstChildElement("fundo");
+	pElement->QueryFloatAttribute("corR",&telaR);
+	pElement->QueryFloatAttribute("corG",&telaG);
+	pElement->QueryFloatAttribute("corB",&telaB);
+    pElement = pRoot->FirstChildElement("circulo");
+    pElement->QueryFloatAttribute("corR", &cR);
+	pElement->QueryFloatAttribute("corG", &cG);
+	pElement->QueryFloatAttribute("corB", &cB);
+    pElement->QueryIntAttribute("raio", &raio);
+    pElement = pRoot->FirstChildElement("circuloModelo");
+    pElement->QueryFloatAttribute("corR", &cnR);
+	pElement->QueryFloatAttribute("corG", &cnG);
+	pElement->QueryFloatAttribute("corB", &cnB);
+    
+    pElement->QueryFloatAttribute("corSobreposicaoR", &caR);
+	pElement->QueryFloatAttribute("corSobreposicaoG", &caG);
+	pElement->QueryFloatAttribute("corSobreposicaoB", &caB);
+	
+    
+}
+
+
 //****MAIN*****//
+
 
 
 int main(int argc, char**argv)
 {
+    read_xml(argv[1]);
 	//Modos da janela
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE |GLUT_RGB);
 	glutInitWindowSize(wzx, wzy); 
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("aiai");
+	glutCreateWindow(titulo.c_str());
 	
 
 	//Callbacks	
@@ -83,11 +122,12 @@ void display(void){
     
     
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0,0,1.0);
-    
+	glColor3f(telaR,telaG,telaB);
     glBegin(GL_LINE_LOOP);
-	glColor3f(0.13, 0.55, 40);
-	raio = 0.1;
+    if (impedido){
+        glColor3f(caR, caG, caB);
+    }
+    else glColor3f(cnR, cnG, cnB);
 	for(i = 0; i < circle_points; i++) {
 		angle = (2*PI*i)/circle_points;
 		glVertex2f(gxm+raio*cos(angle),gym+raio*sin(angle));
@@ -97,8 +137,7 @@ void display(void){
             if(check){
                 
                     glBegin(GL_POLYGON);
-                    glColor3f(0.13, 0.55, 40);
-                    raio = 0.1;
+                    glColor3f(cR, cG, cB);
                     for(i = 0; i < circle_points; i++) {
                         angle = (2*PI*i)/circle_points;
                         glVertex2f(pos_x[j]+raio*cos(angle),pos_y[j]+raio*sin(angle));
@@ -111,9 +150,12 @@ void display(void){
 }
 
 void cursorFormat(int x, int y){
-    y = wzy - y;
-    gxm = (float) x/ wzx;
-    gym = (float) y/ wzy;
+    gxm = (float) x;
+    gym = (float) y;
+    if(distancia(x,y)){
+        impedido = false;
+    }
+    else impedido = true;
 	glutPostRedisplay();
     
 }
@@ -123,7 +165,7 @@ void init (void)
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0,-1.0, 1.0);
+	glOrtho(0.0f, wzx,  wzy, 0.0f, 0.0f, 1.0f);
 }
 
 
@@ -131,27 +173,27 @@ void init (void)
 void mouse(int button, int  state, int x, int y){
        
         if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+            dragCirc = false;
             check = 1;
             if(distancia(x,y)){
-                y = wzy - y;
-                pos_x.push_back((float)x/ wzx);
-                pos_y.push_back((float)y/ wzy);
+                pos_x.push_back((float)x);
+                pos_y.push_back((float)y);
             }                       
         }
         else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN){
             dragCirc = true;
         }
+        else{
+            dragCirc = false;
+        }
         glutPostRedisplay();
 }
 
 bool distancia(int x, int y){
-    float yt = wzy - y;
-    float xt = ((float)x/ wzx);
-    yt = ((float)yt/ wzy);
     
     for(int j=0; j<(pos_x.size()); j++){
         
-        if(sqrt( (xt-pos_x[j])*(xt-pos_x[j]) + (yt-pos_y[j])*(yt-pos_y[j]) ) < 2*raio){
+        if(sqrt( (x-pos_x[j])*(x-pos_x[j]) + (y-pos_y[j])*(y-pos_y[j]) ) < 2*raio){
             
             return false;
         }
@@ -160,11 +202,17 @@ bool distancia(int x, int y){
     
 }
 
+
+
 void arrasta(int x, int y){
     int circulo;
-    float yt = wzy - y;
-    float xt = ((float)x/ wzx);
-    yt = ((float)yt/ wzy);
+    float yt = ((float)y);
+    float xt = ((float)x);
+    
+    gxm = (float) x;
+    gym = (float) yt;
+    
+    yt = ((float)yt);
     
     for(int j=0; j<(pos_x.size()); j++){
         
@@ -173,9 +221,10 @@ void arrasta(int x, int y){
         }
     }
     if(dragCirc && (circulo < pos_x.size()) && (circulo >= 0)){
-            y = wzy - y;
-            pos_x[circulo] = (float)x/ wzx;
-            pos_y[circulo] = (float)y/ wzy;
+            y = y;
+            pos_x[circulo] = (float)x;
+            pos_y[circulo] = (float)y;
     }
+
     glutPostRedisplay();
 }
