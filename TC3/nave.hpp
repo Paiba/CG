d36980cb5d32ba872c7d4bbf4c56789023569c15
paiba <pedropaivaladeira@hotmail.com>
2019-10-27 17:58:35 -0300

    Canhão em progresso, Teletransporte bugadaço.

--- TC3/nave.hpp
+++ TC3/nave.hpp
@@ -9,11 +9,15 @@ public:
     int id;
     string cor;
 	float tan_now;
+	float desl_can;
 
     Nave();
     void setAtt(float x, float y, int iD, float raio, string fill, float tangente);
 	void desenhaCirculoEli(float raio, float r, float g, float b, float elip);
-	void desenhaHelix(float a, float b);	
+	void desenhaHelix(float a, float b);
+	void desenhaDet(float a, float b);
+	void desenhaAsa(float a, float b);	
+	void desenhaCanhao(float a, float b);
 	void desenhaNave();
 };
 
